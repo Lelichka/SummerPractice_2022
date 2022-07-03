aff98f47e2cb8152fdd6f62e62287a8aba83c8c8
Lelichka <olmenua2003@gmail.com>
2022-07-03 15:21:55 +0300

    home task day3

---
+++ app/src/main/java/com/itis/summerpractice_hometask3/Landscape.kt
@@ -0,0 +1,8 @@
+package com.itis.summerpractice_hometask3
+
+data class Landscape(
+    val id:Int,
+    val name:String,
+    val city:String,
+    val description: String,
+    val url: String = "")
